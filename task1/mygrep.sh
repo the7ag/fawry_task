5e88c881af0bbfd7099779eff595acc21df07893
the7ag <mohamed.dead1543@gmail.com>
2025-04-27 21:09:30 +0300

    Refactor argument parsing in mygrep.sh to use getopts, improve error handling for missing arguments, and enhance usage message clarity

--- task1/mygrep.sh
+++ task1/mygrep.sh
@@ -6,46 +6,73 @@ searchingPattern=""
 filePath=""
 
 usage() {
-    echo "Usage: $0 [-n] [-v] <PATTERN> <FILE>"
-    echo "Search for PATTERN in FILE."
-    echo ""
-    echo "options:"
-    echo "  -n: Show line numbers"
-    echo "  -v: Invert match"
-    echo "--help: Show this help message and exit"
-    echo ""
-    echo "examples:"
-    echo "  $0 hello file.txt"
-    echo "  $0 -n hello file.txt"
-    echo "  $0 -vn hello file.txt"
-    echo "  $0 -n -v hello file.txt"
+    cat <<EOF
+Usage: $0 [-n] [-v] <PATTERN> <FILE>
+Search for PATTERN in FILE.
+
+options:
+    -n: Show line numbers
+    -v: Invert match
+    --help: Show this help message and exit
+
+examples:
+    $0 hello file.txt
+    $0 -n hello file.txt
+    $0 -vn hello file.txt
+    $0 -n -v hello file.txt
+EOF
     exit 1
 }
 
-if [ "$1" == "--help" ]; then
-    usage
-fi
+# if [ "$1" == "--help" ]; then
+#     usage
+# fi
 
-while [[ "$1" == -* ]]; do 
-  if [[ "$1" == *"n"* ]]; then 
-      showLineNumber=true
-  fi
-  if [[ "$1" == *"v"* ]]; then 
-      invertMatch=true
-  fi
-  shift
+# while [[ "$1" == -* ]]; do 
+#   if [[ "$1" == *"n"* ]]; then 
+#       showLineNumber=true
+#   fi
+#   if [[ "$1" == *"v"* ]]; then 
+#       invertMatch=true
+#   fi
+#   shift
+# done
+
+while getopts ":nv" opt; do
+    case $opt in
+        n) showLineNumber=true ;;
+        v) invertMatch=true ;;
+        *) usage ;;
+    esac
 done
+shift $((OPTIND - 1))
+
 
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 [-n] [-v] <PATTERN> <FILE>"
+    if [ "$#" -eq 1 ]; then
+        echo "Error: Missing search pattern or filename." 1>&2
+    else
+        echo "Error: Incorrect number of arguments" 1>&2
+    fi
+    usage
     exit 1
 fi
 
 searchingPattern="$1"
 filePath="$2"
 
+if [ -z "$searchingPattern" ]; then
+    echo "Error: Missing search pattern" 1>&2
+    usage
+    exit 1
+fi
+
 if [ ! -f "$filePath" ]; then
-    echo "Error: file '$filePath' does not exist"
+    echo "Error: file '$filePath' does not exist" 1>&2
+    exit 1
+fi
+if [ ! -r "$filePath" ]; then
+    echo "Error: file '$filePath' is not readable" 1>&2
     exit 1
 fi
 
