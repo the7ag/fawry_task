129da41ee198a3f8be3bd5d8ac7e5bf5a12adf64
the7ag <mohamed.dead1543@gmail.com>
2025-04-27 20:11:54 +0300

    Add options for line numbering and inverted matching in mygrep.sh

--- task1/mygrep.sh
+++ task1/mygrep.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <PATTERN> <FILE>"
+showLineNumber=false
+invertMatch=false
+
+while [[ "$1" == -* ]]; do 
+  if [[ "$1" == *"n"* ]]; then 
+      showLineNumber=true
+  fi
+  if [[ "$1" == *"v"* ]]; then 
+      invertMatch=true
+  fi
+  shift
+done
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 [-n] [-v] <PATTERN> <FILE>"
     exit 1
 fi
 
@@ -14,11 +27,35 @@ if [ ! -f "$filePath" ]; then
 fi
 
 patternLower=$(echo "$searchingPattern" | tr '[:upper:]' '[:lower:]')
+lineNumber=0
 
 while read line; do
+    lineNumber=$((lineNumber + 1))
     lineLower=$(echo "$line" | tr '[:upper:]' '[:lower:]')
+
+    match=false
     if [[ "$lineLower" == *"$patternLower"* ]]; then
-        echo "$line"
+        match=true;
+    fi
+
+    printLine=false
+    if [ "$invertMatch" = true ]; then
+        if [ ! "$match" = true ]; then
+            printLine=true
+        fi
+    else
+        if [ "$match" = true ]; then
+            printLine=true
+        fi
+    fi
+
+    if [ "$printLine" = true ]; then
+        if [ "$showLineNumber" = true ]; then   
+            echo "$lineNumber: $line"
+        else
+            echo "$line"
+        fi
     fi
 done < "$filePath"
+
 exit 0
