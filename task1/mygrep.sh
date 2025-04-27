3cb2413cc9b08a504cfffbb29a50b4b7626cb98e
the7ag <mohamed.dead1543@gmail.com>
2025-04-27 19:01:31 +0300

    Enhance mygrep.sh to include file existence check and improve usage message format

--- task1/mygrep.sh
+++ task1/mygrep.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 PATTERN FILE"
+    echo "Usage: $0 <PATTERN> <FILE>"
     exit 1
 fi
 
 searchingPattern="$1"
 filePath="$2"
+
+if [ ! -f "$filePath" ]; then
+    echo "Erro: file '$filePath' does not exist"
+    exit 1
+fi
+
 echo "Searching for '$searchingPattern' in '$filePath'"
